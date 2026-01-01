3aea92397ac72f0b70205cb376c22b911e15d1a1
Matthew Quinn <iam@matthewquinn.me>
2026-01-01 17:18:17 +1300

    Adds mod file at dist root

---
+++ lib/mod.ts
@@ -0,0 +1,33 @@
+
+export {
+  PolicyDirective,
+  Directory,
+  FilesByURL,
+  ReferenceDetails,
+  ReferenceParser,
+  ReferencePreprocessor,
+} from './static/types.ts';
+
+export {
+  StaticExtensionArgs,
+  StaticExtension,
+} from './static/static-extension.ts';
+
+export {
+  CSSReferenceParserArgs,
+  CSSReferenceParser,
+} from './static/css-parser.ts';
+
+export {
+  JSReferenceParserArgs,
+  JSReferenceParser,
+} from './static/js-parser.ts';
+
+export {
+  HTMLParserArgs,
+  HTMLParser,
+} from './static/html-parser.ts';
+
+export {
+  TSReferencePreprocessor,
+} from './static/ts-preprocessor.ts';
